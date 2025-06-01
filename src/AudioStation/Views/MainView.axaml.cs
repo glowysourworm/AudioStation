8976a3564d249ea62f89c53b0d079e44123cf8e7
glowysourworm <aniv.sm.esports@gmail.com>
2025-05-31 20:49:36 -0500

    init

---
+++ src/AudioStation/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AudioStation.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/AudioStation/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AudioStation.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
