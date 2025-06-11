43f55fd9038eeb5c3d493613daaf624ee0f79de1
glowysourworm <aniv.sm.esports@gmail.com>
2025-06-11 16:11:33 -0500

    More rough draft styling: the current look is very childish looking; but flattened to a rough-draft
    The tab items will eventually go away. So, a lot of the layout will be fixed once the functionality is complete. Then, the various views and modes can be completed.

---
+++ src/AudioStation/Controls/TabItemPressable.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AudioStation.Controls
+{
+    public class TabItemPressable : TabItem
+    {
+        public bool IsPressed
+        {
+            get { return (bool)GetValue(IsPressedProperty); }
+            set { SetValue(IsPressedProperty, value); }
+        }
+
+        public TabItemPressable()
+        {
+        }
+
+        public static readonly DependencyProperty IsPressedProperty =
+           DependencyProperty.Register("IsPressed", typeof(bool), typeof(TabItemPressable), new PropertyMetadata(false));
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+            IsPressed = true;
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            IsPressed = false;
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+            IsPressed = false;
+        }
+    }
+}
