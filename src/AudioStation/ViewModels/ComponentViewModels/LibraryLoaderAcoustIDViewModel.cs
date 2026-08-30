a4d7f5d8861a8b641e876834b368b37cecba253b
glowysourworm <ryanddolan@hotmail.com>
2026-08-29 21:49:13 -0500

    Un-commented and updated the AcoustID library loader component

--- src/AudioStation/ViewModels/ComponentViewModels/LibraryLoaderAcoustIDViewModel.cs
+++ src/AudioStation/ViewModels/ComponentViewModels/LibraryLoaderAcoustIDViewModel.cs
@@ -1,7 +1,16 @@
-﻿using AudioStation.Core.Model.Interface;
+﻿using AudioStation.Core.Component.LibraryLoaderComponent;
+using AudioStation.Core.Database.AudioStationDatabase;
+using AudioStation.Core.Model;
+using AudioStation.Core.Model.Interface;
+using AudioStation.Core.Utility;
 using AudioStation.EventHandler;
 using AudioStation.Service.Interface;
+using AudioStation.Utility;
 using AudioStation.ViewModels.ComponentViewModels.LibraryLoaderViewModels;
+using AudioStation.ViewModels.ComponentViewModels.LibraryLoaderViewModels.Load;
+using AudioStation.ViewModels.ComponentViewModels.LibraryLoaderViewModels.Output;
+
+using Microsoft.Extensions.Logging;
 
 using SimpleWpf.IocFramework.Application.Attribute;
 using SimpleWpf.IocFramework.EventAggregation;
@@ -11,20 +20,6 @@ namespace AudioStation.ViewModels.ComponentViewModels
     [IocExportDefault]
     public class LibraryLoaderAcoustIDViewModel : LibraryLoaderComponentViewModelBase
     {
-        string _musicFolder;
-        string _downloadFolder;
-
-        public string MusicFolder
-        {
-            get { return _musicFolder; }
-            set { this.RaiseAndSetIfChanged(ref _musicFolder, value); }
-        }
-        public string DownloadFolder
-        {
-            get { return _downloadFolder; }
-            set { this.RaiseAndSetIfChanged(ref _downloadFolder, value); }
-        }
-
         [IocImportingConstructor]
         public LibraryLoaderAcoustIDViewModel(IIocEventAggregator eventAggregator, ILibraryLoaderService libraryLoaderService)
             : base(eventAggregator, libraryLoaderService)
@@ -34,44 +29,54 @@ namespace AudioStation.ViewModels.ComponentViewModels
 
         protected override void InitializeComponent(IAudioStationConfiguration configuration, DialogEventHandlers.DialogProgressHandler progressHandler)
         {
-            //this.MusicFolder = Path.Combine(configuration.DirectoryBase, configuration.MusicSubDirectory);
-            //this.DownloadFolder = configuration.ImportFolder;
+            try
+            {
+                // Procedure
+                //
+                // 1) Loop through all library directories (+ staging and download)
+                // 2) Build directory tree (using DirectoryTreeLoader)
+                // 3) Build work items
 
-            //try
-            //{
-            //    // Load Directory
-            //    var directoryTree = DirectoryTreeLoader.Load(this.MusicFolder, "*.mp3");
+                // Initialize Work Items
+                this.WorkItems.Clear();
 
-            //    // Initialize Work Items
-            //    this.WorkItems.Clear();
+                foreach (var libraryDirectory in configuration.LibraryDirectories.Union(new LibraryDirectory[]
+                {
+                    configuration.StagingFolder,
+                    configuration.DownloadFolder
+                }))
+                {
+                    // Load Directory
+                    var directoryTree = DirectoryTreeLoader.Load(libraryDirectory.Directory, "*.mp3");
 
-            //    directoryTree.RecurseForEach(entry =>
-            //    {
-            //        if (!entry.NodeValue.IsDirectory)
-            //            this.WorkItems.Add(new LibraryWorkItemViewModel()
-            //            {
-            //                HasErrors = false,
-            //                IsCompleted = false,
-            //                LoadType = LibraryLoadType.AcoustID,
-            //                Load = new LibraryLoaderLoadViewModel()
-            //                {
-            //                    DisplayText = entry.NodeValue.FullPath,
-            //                    Data = new LibraryLoaderFileLoadViewModel(entry.NodeValue.FullPath, entry.NodeValue.ShortPath)
-            //                },
-            //                Output = new LibraryLoaderOutputViewModel()
-            //                {
-            //                    Output = new LibraryLoaderEntitySetOutputViewModel<AcoustIDLookupResult>()
-            //                },
-            //                InProgress = false,
-            //                Progress = 0
-            //            });
-            //    });
-            //}
-            //catch (Exception ex)
-            //{
-            //    ApplicationHelpers.Log("Error loading import files:  {0}", LogLevel.Error, ex, ex.Message);
-            //    throw ex;
-            //}
+                    directoryTree.RecurseForEach(entry =>
+                    {
+                        if (!entry.NodeValue.IsDirectory)
+                            this.WorkItems.Add(new LibraryWorkItemViewModel()
+                            {
+                                HasErrors = false,
+                                IsCompleted = false,
+                                LoadType = LibraryLoadType.AcoustID,
+                                Load = new LibraryLoaderLoadViewModel()
+                                {
+                                    DisplayText = entry.NodeValue.FullPath,
+                                    Data = new LibraryLoaderFileLoadViewModel(entry.NodeValue.FullPath, entry.NodeValue.ShortPath)
+                                },
+                                Output = new LibraryLoaderOutputViewModel()
+                                {
+                                    Output = new LibraryLoaderEntitySetOutputViewModel<AcoustIDLookupResult>()
+                                },
+                                InProgress = false,
+                                Progress = 0
+                            });
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                ApplicationHelpers.Log("Error loading import files:  {0}", LogLevel.Error, ex, ex.Message);
+                throw ex;
+            }
         }
     }
 }
